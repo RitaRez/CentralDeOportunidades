440a9266f1235fc4d0cc41bdb23f1c1ce08e3d04
RitaRez <ritarezborges@gmail.com>
2018-06-27 06:25:25 -0300

    iniciei

--- vagas.php
+++ vagas.php
@@ -3,24 +3,70 @@
     <head>
         <meta charset="utf-8">
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.1.0/css/all.css" integrity="sha384-lKuwvrZot6UHsBSfcMvOkWwlCMgc0TaWr+30HWe3a4ltaBwTZhyTEggF5tJv8tbt" crossorigin="anonymous">
+        <link rel="stylesheet" href="/css/vagas.css">
+        <script src="script/nav.js" type="text/javascript"></script>
         <title></title>
     </head>
     <body>
+        <!-- NavBar -->
         <?php include "nav.inc" ?>
-        <div class="pl-5">
-            <img src="assets/profile.jpg" alt="">
-            <img src="assets/profile.jpg" alt="">
 
-            <img src="assets/profile.jpg" alt="">
+        <!-- Search bar -->
+        <nav id="searchBar" class="py-3 container-fluid">
+            <div class="row">
+                <button class="ml-4" id="navOpener" onclick="openNav()"><i class="fas fa-2x fa-bars"></i></button>
+                <div class="offset-0 col-8 offset-xs-1 offset-sm-1 offset-md-2 offset-lg-2 col-xs-6 col-sm-7 col-md-8 col-lg-8 input-group">
+                  <div class="input-group-prepend">
+                    <span class="input-group-text" id="search"><i class="fas fa-search"></i></span>
+                  </div>
+                  <input placeholder="Procure uma vaga" type="text" class="form-control" aria-label="Username" aria-describedby="basic-addon1">
+                </div>
+            </div>
+        </nav>
 
-            <img src="assets/profile.jpg" alt="">
-            <img src="assets/profile.jpg" alt="">
-            <img src="assets/profile.jpg" alt="">
 
-            <img src="assets/profile.jpg" alt="">
-            <img src="assets/profile.jpg" alt="">
 
-            <img src="assets/profile.jpg" alt="">
+        <!-- Cards Gallery -->
+        <div class="mx-3 mb-5 mt-3 cards">
+            <div class="container-fluid">
+                <div class="row pt-4">
+                    <div class="mt-4 col-xs-12 col-sm-12 col-md-6 col-lg-4 col-xl-3">
+                        <?php include "card1.inc" ?>
+                    </div>
+                    <div class="mt-4 col-xs-12 col-sm-12 col-md-6 col-lg-4 col-xl-3">
+                        <?php include "card2.inc" ?>
+                    </div>
+                    <div class="mt-4  col-xs-12 col-sm-12 col-md-6 col-lg-4 col-xl-3">
+                        <?php include "card1.inc" ?>
+                    </div>
+                    <div class="mt-4  col-xs-12 col-sm-12 col-md-6 col-lg-4 col-xl-3">
+                        <?php include "card2.inc" ?>
+                    </div>
+                    <div class="mt-4 col-xs-12 col-sm-12 col-md-6 col-lg-4 col-xl-3">
+                        <?php include "card1.inc" ?>
+                    </div>
+                    <div class="mt-4 col-xs-12 col-sm-12 col-md-6 col-lg-4 col-xl-3">
+                        <?php include "card2.inc" ?>
+                    </div>
+                    <div class="mt-4  col-xs-12 col-sm-12 col-md-6 col-lg-4 col-xl-3">
+                        <?php include "card1.inc" ?>
+                    </div>
+                    <div class="mt-4  col-xs-12 col-sm-12 col-md-6 col-lg-4 col-xl-3">
+                        <?php include "card2.inc" ?>
+                    </div>
+                    <div class="mt-4 col-xs-12 col-sm-12 col-md-6 col-lg-4 col-xl-3">
+                        <?php include "card1.inc" ?>
+                    </div>
+                    <div class="mt-4 col-xs-12 col-sm-12 col-md-6 col-lg-4 col-xl-3">
+                        <?php include "card2.inc" ?>
+                    </div>
+                    <div class="mt-4  col-xs-12 col-sm-12 col-md-6 col-lg-4 col-xl-3">
+                        <?php include "card1.inc" ?>
+                    </div>
+                </div>
+            </div>
+
         </div>
 
 
