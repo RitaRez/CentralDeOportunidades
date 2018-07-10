c54420f9778e5fda8cf354aedd8395ef060dfc51
ritarez <ritarezborges@gmail.com>
2018-07-10 16:41:36 -0300

    adicionei modal

--- php/vagas.php
+++ php/vagas.php
@@ -30,17 +30,29 @@
                       <div class="modal-dialog modal-dialog-centered" role="document">
                         <div class="modal-content">
                           <div class="modal-header">
-                            <h5 class="modal-title" id="exampleModalLongTitle">Modal title</h5>
+                            <h5 class="modal-title" id="exampleModalLongTitle">Adicionar vaga</h5>
                             <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                               <span aria-hidden="true">&times;</span>
                             </button>
                           </div>
                           <div class="modal-body">
-                            ...
+                            <form class="container-fluid" action="index.html" method="post">
+                                <input id="nome" type="text" name="" value="" placeholder="Nome da vaga">
+                                <input id="Empresa" type="text" name="" value="" placeholder="Empresa">
+                                <input id="dedicacao" type="number" name="" value="" placeholder="Dedicação" >
+                                <input id="descricao" type="textarea" name="" value="" placeholder="Descrição da vaga">
+                                <div class="row">
+                                    <div class="">
+                                        <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                                        <label class="form-check-label" for="exampleCheck1">Informática</label>
+                                    </div>
+                                </div>
+
+                            </form>
                           </div>
                           <div class="modal-footer">
-                            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                            <button type="button" class="btn btn-primary">Save changes</button>
+                            <button type="button" class="btn btn-secondary" data-dismiss="modal">Fechar</button>
+                            <button type="button" class="btn btn-primary">Adicionar vaga</button>
                           </div>
                         </div>
                       </div>
